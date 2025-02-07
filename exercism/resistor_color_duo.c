5e7d898b51613ad60bf5b2ff631b660d63d25fd5
joaofsoares <joaofsoares@yahoo.com>
2025-02-07 23:24:16 +0000

    format

--- exercism/resistor_color_duo.c
+++ exercism/resistor_color_duo.c
@@ -4,14 +4,14 @@
 
 #include "resistor_color_duo.h"
 
-uint16_t color_code(resistor_band_t* resistor) {
+uint16_t color_code(resistor_band_t *resistor) {
 
-    long unsigned int * t = malloc(2 * sizeof(long unsigned int));
-    t[0] = resistor[0];
-    t[1] = resistor[1];
+  long unsigned int *t = malloc(2 * sizeof(long unsigned int));
+  t[0] = resistor[0];
+  t[1] = resistor[1];
 
-    char str[3];
-    sprintf(str, "%lu%lu", t[0], t[1]);
+  char str[3];
+  sprintf(str, "%lu%lu", t[0], t[1]);
 
-    return atoi(str);
+  return atoi(str);
 }
