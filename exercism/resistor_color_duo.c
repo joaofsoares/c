845ec28e4271060c95a71454f0182d0416e46764
João Felipe Soares <joaofsoares@yahoo.com>
2025-02-07 13:18:38 +0000

    Create resistor_color_duo.c

---
+++ exercism/resistor_color_duo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "resistor_color_duo.h"
+
+uint16_t color_code(resistor_band_t* resistor) {
+
+    long unsigned int * t = malloc(2 * sizeof(long unsigned int));
+    t[0] = resistor[0];
+    t[1] = resistor[1];
+
+    char str[3];
+    sprintf(str, "%lu%lu", t[0], t[1]);
+
+    return atoi(str);
+}
